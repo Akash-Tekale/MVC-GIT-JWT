088cf8b5914c2a10e8c9b35838b7d1b3bdaa4a3b
Akash Tekale <akashtekale566@gmail.com>
2025-03-15 16:52:52 +0530

    delete / user  route db

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const DB = () =>{
     try {
-        mongoose.connect('mongodb+srv://akash:akash@cluster0.wkcwr.mongodb.net/users');
+        mongoose.connect('mongodb+srv://akash:akash@cluster0.wkcwr.mongodb.net/');
         console.log("Mongodb connected ");
     } catch (error) {   
         console.log("Mongo ERROR ! ! ! "+error);
