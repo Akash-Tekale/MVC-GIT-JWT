79141c719f04d7de16e70d34e2b5bbee815a2909
Akash Tekale <akashtekale566@gmail.com>
2025-03-15 21:02:02 +0530

    chenged mongo url

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const DB = () =>{
     try {
-        mongoose.connect('mongodb://localhost:27017/demoapi');
+        mongoose.connect('mongodb+srv://akash:akash@cluster0.wkcwr.mongodb.net/');
         console.log("Mongodb connected ");
     } catch (error) {   
         console.log("Mongo ERROR ! ! ! "+error);
